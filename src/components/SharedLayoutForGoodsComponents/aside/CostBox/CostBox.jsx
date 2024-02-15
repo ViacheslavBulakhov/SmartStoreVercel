3ed0fd2c08c4122fb7aa7db154efd5583e8371c6
ViacheslavBulakhov <95303344+ViacheslavBulakhov@users.noreply.github.com>
2024-02-15 19:33:42 +0200

    fix costBox

--- src/components/SharedLayoutForGoodsComponents/aside/CostBox/CostBox.jsx
+++ src/components/SharedLayoutForGoodsComponents/aside/CostBox/CostBox.jsx
@@ -17,9 +17,12 @@ const CostBox = () => {
       setRangeValues([Math.min(...arr), Math.max(...arr)]);
     }
     const array = goods.map(item => Number(item.amount));
+    console.log(array);
 
     findMaxMin(array);
   }, [goods]);
+
+  const isRange = rangeValues?.length > 0 && rangeValues[0] !== rangeValues[1];
 
   return (
     rangeValues && (
@@ -31,6 +34,7 @@ const CostBox = () => {
             aria-label="Ціна"
             value={rangeValues[0]}
             onChange={e =>
+              isRange &&
               setRangeValues([Number(e.target.value), rangeValues[1]])
             }
           />
@@ -43,16 +47,19 @@ const CostBox = () => {
             aria-label="Ціна"
             value={rangeValues[1]}
             onChange={e =>
+              isRange &&
               setRangeValues([rangeValues[0], Number(e.target.value)])
             }
           />
 
           <span>Грн</span>
         </InputsWrap>
-        <RangeSlider
-          rangeValues={rangeValues}
-          setRangeValues={setRangeValues}
-        />
+        {isRange && (
+          <RangeSlider
+            rangeValues={rangeValues}
+            setRangeValues={setRangeValues}
+          />
+        )}
       </FiltersItemWrap>
     )
   );
